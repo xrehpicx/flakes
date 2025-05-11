08bcd33c15b3739ed09974e55cd3010ab091679c
Raj Sharma <r@raj.how>
2025-05-12 00:04:54 +0530

    refactor: use sbctl binary path variable in Secure Boot scripts for consistency

--- modules/linux-secureboot.nix
+++ modules/linux-secureboot.nix
@@ -1,19 +1,22 @@
 { pkgs, lib, ... }:
 
-let sbDir = "/etc/sbctl"; in
+let
+  sbctlBin = "${pkgs.sbctl}/bin/sbctl";
+  sbDir = "/etc/sbctl";
+in
 {
   environment.systemPackages = with pkgs; [ sbctl ];
 
   # generate and enroll Secure Boot keys once, before activation
   system.activationScripts.sbctl-enroll = lib.mkBefore ''
     if [ ! -f ${sbDir}/db.crt ]; then
-      sbctl create-keys --directory ${sbDir}
-      sbctl enroll-keys -m --directory ${sbDir} --yes
+      ${sbctlBin} create-keys --directory ${sbDir}
+      ${sbctlBin} enroll-keys -m --directory ${sbDir} --yes
     fi
   '';
 
   # automatically re-sign every EFI entry in your ESP on each activation
   system.activationScripts.sbctl-sign = lib.mkAfter ''
-    sbctl sign --key-dir ${sbDir} --batch /boot/EFI/nixos/*.efi
+    ${sbctlBin} sign --key-dir ${sbDir} --batch /boot/EFI/nixos/*.efi
   '';
 } 
