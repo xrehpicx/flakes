99f1580f08e2f88b66b1927c2f69c61243552749
Raj Sharma <r@raj.how>
2025-05-12 00:08:54 +0530

    fix: streamline Secure Boot key creation and signing process in Nix configuration

--- modules/linux-secureboot.nix
+++ modules/linux-secureboot.nix
@@ -10,13 +10,13 @@ in
   # generate and enroll Secure Boot keys once, before activation
   system.activationScripts.sbctl-enroll = lib.mkBefore ''
     if [ ! -f ${sbDir}/db.crt ]; then
-      ${sbctlBin} create-keys --directory ${sbDir}
-      ${sbctlBin} enroll-keys -m --directory ${sbDir} --yes
+      ${sbctlBin} create-keys
+      ${sbctlBin} enroll-keys -m --yes
     fi
   '';
 
   # automatically re-sign every EFI entry in your ESP on each activation
   system.activationScripts.sbctl-sign = lib.mkAfter ''
-    ${sbctlBin} sign --key-dir ${sbDir} --batch /boot/EFI/nixos/*.efi
+    ${sbctlBin} sign --batch /boot/nixos/*.efi
   '';
 } 
