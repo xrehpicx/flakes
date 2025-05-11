0e7318963b467b038ce20504cbeca31e27384c92
Raj Sharma <r@raj.how>
2025-05-11 23:31:56 +0530

    feat: add Secure Boot key generation and enrollment scripts

--- modules/linux-secureboot.nix
+++ modules/linux-secureboot.nix
@@ -4,8 +4,16 @@ let sbDir = "/etc/sbctl"; in
 {
   environment.systemPackages = with pkgs; [ sbctl ];
 
+  # generate and enroll Secure Boot keys once, before activation
+  system.activationScripts.sbctl-enroll = lib.mkBefore ''
+    if [ ! -f ${sbDir}/db.crt ]; then
+      sbctl create-keys --directory ${sbDir}
+      sbctl enroll-keys -m --directory ${sbDir} --yes
+    fi
+  '';
+
+  # automatically re-sign every EFI entry in your ESP on each activation
   system.activationScripts.sbctl-sign = lib.mkAfter ''
-    # automatically re-sign every EFI entry in your ESP
     sbctl sign --key-dir ${sbDir} --batch /boot/EFI/nixos/*.efi
   '';
 } 
