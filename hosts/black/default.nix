2dc5ba34081f64852d626717223676fc190665dd
Raj Sharma <r@raj.how>
2025-05-12 13:44:04 +0530

    fix: enable PasswordAuthentication in SSH settings for improved access

--- hosts/black/default.nix
+++ hosts/black/default.nix
@@ -30,7 +30,7 @@
   services.openssh = {
     enable = true;
     settings = {
-      PasswordAuthentication = false;
+      PasswordAuthentication = true;
       PermitRootLogin = "no";
     };
   };
